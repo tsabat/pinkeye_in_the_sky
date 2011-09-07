2d5027d999738d572dc95d97d3b40bd8b8d18b50
Timothy Sabat <timothy.sabat@gmail.com>
2011-09-07 09:27:42 -0700

    Temp commit

--- node/index.js
+++ node/index.js
@@ -1,14 +1,47 @@
 var http = require("http");
-var requests = [];
+var redis = require("redis"),
 
-http.createServer(function(request, response) {
-        requests.push(response);
-}).listen(8000);
+client = redis.createClient();
+var requests = [];
+var strings = [];
+
+http.createServer(
+    function(request, response) {
+        if (request.url === '/favicon.ico') {
+            response.writeHead(200, {'Content-Type': 'image/x-icon'});
+            response.end();
+            console.log('favicon requested');
+        } else {
+            requests.push(response);
+			console.log(requests);
+        }
+
+    }).listen(8000);
+
+function foo() {
+    var d = new Date();
+    var key = "pink-eye-p-" + d.getMinutes();
+    console.log('reading key into array from ' + key);
+    client.smembers(key, function(err, obj) {
+        if (obj) {
+            for (var i = 0; i < obj.length; i++) {
+                strings.push(JSON.parse(obj[i]));
+            }
+        }
+    });
+}
 
 setInterval(function() {
-        while (requests.length) {
-            response = requests.shift();
-            response.writeHead(200, { "Content-Type": "text/plain" });
-            response.end("Hello, World!");
-         }
-}, 2000);
+	foo();
+    while (requests.length) {
+        console.log('sending requests back.');
+        response = requests.shift();
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.end(JSON.stringify(strings));
+		console.log(requests);
+
+    }
+	console.log("Killing requests");
+    strings = [];
+
+}, 5000);
